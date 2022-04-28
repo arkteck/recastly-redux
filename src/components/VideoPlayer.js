a739e0dfcda08e9c9c71036ef038fa181ed3aa4a
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-04-28 14:48:33 -0400

    Add video title and details to video player

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -11,8 +11,8 @@ var VideoPlayer = ({video}) => {
         <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
       </div>
       <div className="video-player-details">
-        <h3></h3>
-        <div></div>
+        <h3>{video.snippet.title}</h3>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
 )};
